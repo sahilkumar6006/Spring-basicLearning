47fee7aa00283fefb65d91c14437a74373280aa7
sahilkumar <sahilgorka123@gmail.com>
2026-01-31 14:47:32 +0530

    added Wild card and added changes

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -7,16 +7,20 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 
 
 @RestController
+@RequestMapping("/api/v1/todos")
 public class TodoController {
 
     private List<Todo> todosList = new ArrayList<>();
@@ -28,12 +32,12 @@ public class TodoController {
 
     }
 
-    @GetMapping("/todos")
+    @GetMapping()
     public List<Todo> getTodos() {
         return todosList;
     }
 
-    @PostMapping("/todos")
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo) {
         todosList.add(newTodo);
@@ -41,7 +45,7 @@ public class TodoController {
     }
     
     
-    @GetMapping("/todos/{todoId}")
+    @GetMapping("/{todoId}")
     public ResponseEntity<Object> getTodosById(@PathVariable Long todoId) {
        for(Todo todo: todosList){
         if(todo.getId() == todoId){
@@ -53,5 +57,34 @@ public class TodoController {
 
     }
     
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<Object> deleteTodo(@PathVariable Long todoId) {
+       for(Todo todo: todosList){
+        if(todo.getId() == todoId){
+            todosList.remove(todo);
+            return ResponseEntity.ok(todo);
+        }
+       }
+
+      return new ResponseEntity<>(Map.of("error", "Todo not found", "id", todoId), HttpStatus.NOT_FOUND);
+
+    }
     
+    @PatchMapping("/{todoId}")
+    public ResponseEntity<?> updateTodo(@PathVariable Long todoId, @RequestBody Todo updatedTodo) {
+       for(Todo todo: todosList){
+        if(todo.getId() == todoId){
+            todo.setTitle(updatedTodo.getTitle());
+            todo.setCompleted(updatedTodo.isCompleted());
+            return ResponseEntity.ok(todo);
+        }
+       }
+
+      return new ResponseEntity<>(Map.of("error", "Todo not found", "id", todoId), HttpStatus.NOT_FOUND);
+
+    }
 }
+
+
+// <?> is a Java generics placeholder called a wildcard.
+// “This ResponseEntity can contain any type of body, I don’t care what it is.”
